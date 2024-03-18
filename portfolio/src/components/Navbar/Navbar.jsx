99f0fe189723b52d3211ed64d35d56686be51c61
JasonR-White23 <jasonshane1998@gmail.com>
2024-03-18 09:50:42 -0600

    Installed font awesome and added content to Navbar.jsx

---
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import './Navbar.css';
+import MobileNav from "./MobileNav/MobileNav";
+
+
+
+const Navbar = () => {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
+    return (
+        <>
+            <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+
+            <nav className="nav-wrapper">
+                <div className="nav-content"> 
+                    <ul>
+                        <li>
+                            <a className="menu-title">
+                                Jason's Portfolio
+                            </a>
+                        </li>
+                        <li>
+                            <a className="menu-item">
+                                About Me
+                            </a>
+                        </li>
+                        <li>
+                            <a className="menu-item">
+                                Education
+                            </a>
+                        </li>
+                        <li>
+                            <a className="menu-item">
+                                Skills
+                            </a>
+                        </li>
+
+                        <button className="contact-btn" onClick={() => {}}>
+                            Contact Me!
+                        </button>
+                    </ul>
+
+                    <button className="menu-btn" onClick={toggleMenu}>
+                        <span>
+                            {openMenu ? "X" : <i className="fa-solid fa-bars"></i>}
+                        </span>
+                    </button>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
