f80dbc17e0b9bbfe270a1b310b9b56b54e042560
JasonR-White23 <jasonshane1998@gmail.com>
2024-03-18 20:10:11 -0600

    Added more content to the AboutMe.jsx file

--- portfolio/src/components/AboutMe/AboutMe.jsx
+++ portfolio/src/components/AboutMe/AboutMe.jsx
@@ -4,11 +4,16 @@ import './AboutMe.css';
 const AboutMe = () => {
   return (
     <>
-        <div className="about-title">
-            <h1 >
-              About Me:
+        <div className="about-wrapper">
+            <h1 className="about-title">
+              About Me
             </h1>
             <hr/>
+
+            <p className="about-content">
+                My name is Jason White, I am a young developer from Alberta, Canada.
+            </p>
+
         </div>
 
 
