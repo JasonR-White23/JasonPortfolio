036e7629790cbe97a45a8de091472c9ffd9fe810
JasonR-White23 <jasonshane1998@gmail.com>
2024-03-22 08:39:26 -0600

    Create and add conent to the Skills.jsx file

---
+++ portfolio/src/components/Skills/Skills.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Skills.css'
+
+const Skills = () => {
+  return (
+    <>
+        <div className="skills-wrapper">
+
+            <h1>
+                Skills
+                <hr/>
+            </h1>
+
+            
+        </div>
+    </>
+  )
+}
+
+export default Skills
