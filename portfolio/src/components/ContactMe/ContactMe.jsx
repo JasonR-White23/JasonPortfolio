fa17876e91aec4e8b34eaf721cb5c482f9ff8013
JasonR-White23 <jasonshane1998@gmail.com>
2024-03-25 09:24:20 -0600

    Added the ContactMe.jsx file, added some content and some styling

--- portfolio/src/components/ContactMe/ContactMe.jsx
+++ portfolio/src/components/ContactMe/ContactMe.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import './ContactMe.css'
+import ContactInfoCard from './ContactInfoCard/ContactInfoCard'
 
 const ContactMe = () => {
   return (
     <>
-        <section className="contact-container">
-            <h5>
-                Contact Me
-            </h5>
-            <div className="contact-content">
-                <div style={{ flex: 1 }}>
-
+        <div className="contact-wrapper">
+            <section className="contact-container">
+                <h5>
+                    Contact Me
+                </h5>
+                <div className="contact-content">
+                    <div style={{ flex: 1 }}>
+                        <ContactInfoCard
+                            iconUrl="/assets/images/email-icon.png"
+                            text="jasonshane1998@gmail.com"
+                        />
+                        
+                        <ContactInfoCard
+                            iconUrl="/assets/images/linkedin-icon.png"
+                            text="https://www.linkedin.com/in/jason-white23"
+                        />
+                    </div>
+                    <div style={{ flex: 1 }}>
+                        
+                    </div>
                 </div>
-                <div style={{ flex: 1 }}>
-
-                </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </>
   )
 }
