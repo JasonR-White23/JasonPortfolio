02962b29c95d11d4e48351b818f2fcb22c068bff
JasonR-White23 <jasonshane1998@gmail.com>
2024-04-10 10:51:06 -0600

    Added more css to the photo of myself using AboutMe.css

--- portfolio/src/components/AboutMe/AboutMe.jsx
+++ portfolio/src/components/AboutMe/AboutMe.jsx
@@ -12,6 +12,7 @@ const AboutMe = () => {
 
           <img src='/assets/images/jason-01.jpeg' alt='A photo of myself on a restaurant patio' class="picture"/>
           <div className="about-content">
+          
             <p>
               My name is Jason White, I am an only child born in a small town in Alberta, Canada.
             </p>
